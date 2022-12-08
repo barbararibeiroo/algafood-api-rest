37397f9888c804926caf5922f573f9b9d8e677fa
barbararibeiroo <barbararibeiro18@gmail.com>
2022-12-07 21:53:33 -0300

    Projeto API rest - algaworks

---
+++ src/main/java/com/algaworks/algalog/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.algaworks.algalog.domain.exception;
+
+public class EntidadeNaoEncontradaException extends NegocioException {
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
